70180d947bed5381e76882cf4390ee596b60f788
Davi Souza Santos <davii.clink@gmail.com>
2023-05-21 03:57:04 -0300

    tela inicial feita

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, ImageBackground } from 'react-native';
-import { styled } from 'nativewind'
+import { Text, View, Image, ImageBackground, TouchableOpacity } from 'react-native';
 
-import { NativeWindStyleSheet } from "nativewind";
+import { styled } from 'nativewind';
+
+import { NativeWindStyleSheet} from "nativewind";
 NativeWindStyleSheet.setOutput({
   default: "native",
 });
@@ -11,19 +12,17 @@ import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold
-} from '@expo-google-fonts/roboto'
+} from '@expo-google-fonts/roboto';
 
 import {
   BaiJamjuree_700Bold
-} from '@expo-google-fonts/bai-jamjuree'
+} from '@expo-google-fonts/bai-jamjuree';
 
 import blurBg from './assets/bgblur.png';
-import Stripes from './assets/stripes.svg'
-import NLWlogo from './assets/logo.svg'
+import stripes from './components/stripes';
+import Logo from './components/logo';
 
-
-
-const StyledStripes = styled(Stripes)
+const StyledStripes = styled(stripes)
 
 export default function App() {
 
@@ -31,20 +30,29 @@ export default function App() {
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold
-  })
+  });
 
   if (!hasLoadedFonts) {
     return null
-  }
+  };
+
   return (
     
-    <ImageBackground source={blurBg} className="relative items-center outline flex-1 bg-gray-900"
+    <ImageBackground source={blurBg} className="relative items-center justify-center outline flex-1 bg-gray-900 px-8 py-10"
       imageStyle={{ position: 'absolute', left: '-100%' }}>
+        
+        <View className='justify-center items-center flex-1 space-y-6 gap-6'>
+            <Logo />
+            <Text className='text-gray-50 font-title text-3xl leading-tight'>Sua cápsula do tempo</Text>
+
+              <Text className='text-gray-100 leading-relaxed  justify-center items-center font-body text-base text-center'>Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!</Text>
       
-      <View className='justify-center items-center '>
-        <NLWlogo />
-      </View>
-      <StatusBar style="auto" />
+              <TouchableOpacity activeOpacity={0.7} className='bg-green-500 text-black w-64 h-9 rounded-full font-aut text-base font-semibold uppercase items-center justify-center'>Começar a cadastrar
+              </TouchableOpacity>
+         </View>
+
+         <Text className='text-gray-200 leading-relaxed text-center'>Feito com 💜 no NLW da Rocketseat</Text>
+        <StatusBar style="auto" />
     </ImageBackground>
   );
-}
+};
